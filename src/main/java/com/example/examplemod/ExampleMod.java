c3bd0a7c4ca9bcf7ec23f8d8e091dc324ceb9a2f
zlivvy <zionolivia@gmail.com>
2014-10-21 20:14:24 -0600

    added preInit
    fixed imports and added GameRegistry

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,15 +1,25 @@
 package com.example.examplemod;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	Item ruby = new Item();
+    	ruby.setUnlocalizedName("ruby");
+    	ruby.setCreativeTab(CreativeTabs.tabMaterials);
+    	GameRegistry.registerItem(ruby, "ruby");
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
