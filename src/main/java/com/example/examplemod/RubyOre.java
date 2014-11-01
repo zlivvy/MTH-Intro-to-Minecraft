94bcc0d83375e97e1c15749bd5cb56412482c409
zlivvy <zionolivia@gmail.com>
2014-11-01 12:10:30 -0600

    Added RubyOre Class
    imported texture, added name entry to lang file, item method broken

---
+++ src/main/java/com/example/examplemod/RubyOre.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+import net.minecraft.block.BlockOre;
+
+public class RubyOre extends BlockOre {
+	@Override
+	public Item getItemDropped(int par1, Random random, int par3) {
+	}
+
+}
