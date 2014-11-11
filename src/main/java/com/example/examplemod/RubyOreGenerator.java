15528360f0d787c3544ff3e6349dcb17c9b7e07e
zlivvy <zionolivia@gmail.com>
2014-11-11 06:14:12 -0700

    repaired and added comments to rubyoregenerator

--- src/main/java/com/example/examplemod/RubyOreGenerator.java
+++ src/main/java/com/example/examplemod/RubyOreGenerator.java
@@ -15,14 +15,23 @@ public class RubyOreGenerator implements IWorldGenerator {
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		// TODO Auto-generated method stub
 		if (world.provider.dimensionId == 0) {
+			// We want to generate lots of veins in a chunk, 
+			// so we will wrap the whole thing in a for loop 
+			// to repeat the process 128 times.
 			for (int i = 16; i < 30; i++) {
 				// set coordinates
-				int x = chunkX *  + random.nextInt(16);
+				// The x and z coordinates will range from 0 to 15, 
+				// and the y coordinate will range from 15 to 160 (15 + 145), 
+				// which means that the vein can spawn anywhere in the chunk 
+				// between layers 15 to 160.
+				int x = chunkX * 16 + random.nextInt(16);
 				int y = 15 + random.nextInt(145);
 				int z = chunkZ * 16 + random.nextInt(16);
+
 					
 				
-				// make a block
+				// We then use the built in WorldGenMinable class 
+				// to actually create the vein, which for now will be max size 16:
 				new WorldGenMinable(GameRegistry
 					.findBlock(ExampleMod.MODID, "oreRuby"), 16)
 					.generate(world, random, x, y, z);
